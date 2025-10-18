739d3e95057d1a0ac258cbee33dfd704eaca1c15
Guilherme Campos <guimp.campos@gmail.com>
2025-10-17 21:21:08 -0300

    fix: refactor files

--- src/business/service/bank.service.ts
+++ src/business/service/bank.service.ts
@@ -1,6 +1,6 @@
 import { Result } from "../common";
 import { BankTransferError } from "../errors/errors";
 
-export interface BankService {
-    sendTransfer(userId: string, bankAccountId: string, amount: number): Promise<Result<null, BankTransferError>>;
+export abstract class BankService {
+    abstract sendTransfer(userId: string, bankAccountId: string, amount: number): Promise<Result<null, BankTransferError>>;
 }
--- src/business/service/notification.service.ts
+++ src/business/service/notification.service.ts
@@ -1,6 +1,6 @@
 import { BusinessError } from "../errors/errors";
 
-export interface NotificationService {
-    notifyUserOfSuccess(withdrawalId: string): Promise<void>;
-    notifyUserOfFailure(userId: string, accountId: string, withdrawalId: string, errors: BusinessError): Promise<void>;
+export abstract class NotificationService {
+    abstract notifyUserOfSuccess(withdrawalId: string): Promise<void>;
+    abstract notifyUserOfFailure(userId: string, accountId: string, withdrawalId: string, errors: BusinessError): Promise<void>;
 }
